cadff3b17a81de5094edcc084c441741a1c394f5
Markus Gärtner <markusgartner@Markuss-iMac.fritz.box>
2020-06-28 09:52:47 +0200

    Initial commit

---
+++ src/GameFX_Main.java
@@ -0,0 +1,53 @@
+import javafx.animation.AnimationTimer;
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+public class GameFX_Main extends Application
+{
+    public static void main(String[] args)
+    {
+        launch(args);
+    }
+
+    public void start(Stage theStage)
+    {
+        theStage.setTitle( "GameFX" );
+
+        Group root = new Group();
+        Scene theScene = new Scene( root );
+        theStage.setScene( theScene );
+
+        Canvas canvas = new Canvas( 1024, 1024 );
+        root.getChildren().add( canvas );
+
+        GraphicsContext gc = canvas.getGraphicsContext2D();
+
+        /*Image earth = new Image( "earth.png" );
+        Image sun   = new Image( "sun.png" );*/
+        Image space = new Image( "playground.png" );
+
+        final long startNanoTime = System.nanoTime();
+
+        new AnimationTimer()
+        {
+            public void handle(long currentNanoTime)
+            {
+                double t = (currentNanoTime - startNanoTime) / 1000000000.0;
+
+                double x = 232 + 128 * Math.cos(t);
+                double y = 232 + 128 * Math.sin(t);
+
+                // background image clears canvas
+                gc.drawImage( space, 0, 0 );
+                /*gc.drawImage( earth, x, y );
+                gc.drawImage( sun, 196, 196 );*/
+            }
+        }.start();
+
+        theStage.show();    }
+}
